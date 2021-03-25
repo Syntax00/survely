3d174be6a52aa9857c63d2d8645e5532e582bd31
Mohamed Ahmed <m.elsayed@fixedmea.com>
2021-03-25 02:21:30 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/getHi", (request, response) => {
+  response.send({ hi: "User Name" });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT);
